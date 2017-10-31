4974709b6910d7cf004e3cf6a75834cf0d4c3d5e
Rishabh <rishabh.aditya@hotmail.com>
2017-10-31 12:50:20 +0530

    Now looks in a bit of shape

--- freshservice_apiv2.gemspec
+++ freshservice_apiv2.gemspec
@@ -1,17 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'freshservice_apiv2'
-  s.version     = '1.0'
-  s.executables   = s.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
+  s.version     = '0.1.0'
+  s.executables = []
   s.summary     = "A simple way to access freshservice API's"
   s.description = "Wrapper for Freshservice API V2"
-  s.authors     = ["Rishabh Aditya"]
+  s.author     = ["Rishabh Aditya"]
   s.email       = 'rishabh.aditya@hotmail.com'
-  s.files       = [ "lib/freshervice_apiv2.rb",
-                    "lib/tickets.rb"
-                    ]
-  s.require_paths = ["lib"]
+  s.files       = ["lib/freshservice_apiv2.rb"]
   s.homepage    = 'https://github.com/rishabh91/freshservice_apiv2'
   s.license       = 'MIT'
-  s.add_runtime_dependency 'rest-client'
-  s.add_runtime_dependency 'json'
 end
--- lib/freshservice_apiv2.rb
+++ lib/freshservice_apiv2.rb
@@ -1,7 +1,8 @@
 require 'rest-client'
 require 'json'
+require 'base64'
 
-class FreshserviceApiv2
+class FreshServiceApiv2
   attr_accessor :header,:username,:password,:apikey,:baseurl
 
 
@@ -77,4 +78,43 @@ class FreshserviceApiv2
     end
   end
 
+end
+
+class Tickets < FreshServiceApiv2
+
+  TICKET_ENDPOINT = "/api/v2/tickets"
+  TICKET_FIELDS = "/api/v2/ticket_fields"
+  TICKET_CONVERSATIONS = "/api/v2/conversations"
+
+  def initialize(uri)
+    super(uri)
+  end
+
+  def create_ticket(data)
+    response = post(TICKET_ENDPOINT,data.to_json)
+  end
+
+  def view_ticket(id)
+    response = get(TICKET_ENDPOINT + "/#{id}")
+  end
+
+  def view_all_ticket_fields
+    response = get(TICKET_FIELDS)
+  end
+
+  def delete_ticket(id)
+    response = delete(TICKET_ENDPOINT + "/#{id}")
+  end
+
+  def update_ticket(id,data)
+    response = update(TICKET_ENDPOINT + "/#{id}", data.to_json)
+  end
+
+  def add_note(id,data)
+    response = post(TICKET_ENDPOINT + "/#{id}/notes", data.to_json)
+  end
+
+  def add_reply(id,data)
+    response = post(TICKET_ENDPOINT + "/#{id}/reply", data.to_json)
+  end
 end
